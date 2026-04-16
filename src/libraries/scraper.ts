277bf8b03e3ee2be46932e82f80fcaa48812302c
Septian Dwi Anugrah <hitamcoklat.web@gmail.com>
2026-04-16 14:28:36 +0700

    chore: update scrapper user agent

--- src/libraries/scraper.ts
+++ src/libraries/scraper.ts
@@ -3,21 +3,35 @@ import { DOMParser } from '@b-fuze/deno-dom'
 import { MONTH_NAME } from '@/constants/month.ts'
 
 const fetcher = async (year: string) => {
-  const response = await fetch(`https://www.tanggalans.com/kalender-${year}`)
+  const response = await fetch(`https://tanggalans.com/kalender-${year}/`, {
+    headers: {
+      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+      "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+      "cache-control": "no-cache",
+      "pragma": "no-cache",
+      "sec-ch-ua": '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
+      "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
+    }
+  });
 
   if (!response.ok) {
-    throw new Error('Failed to fetch tanggalan')
+    throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
   }
 
-  return await response.text()
+  return await response.text();
 }
 
 export const crawler = async (year: string) => {
-  const html = await fetcher(year)
+  const html = await fetcher(year);
+  console.log("HTML terambil, karakter pertama:", html.substring(0, 100)); // Cek apakah HTML-nya beneran ada
 
-  const dom = new DOMParser().parseFromString(html, 'text/html')
-
-  console.log('checkDOM', dom)
+  const dom = new DOMParser().parseFromString(html, 'text/html');
+  if (!dom) {
+    console.error("DOM Gagal di parse!");
+  } else {
+    const testNode = dom.querySelector('article');
+    console.log("Apakah ada tag article?", !!testNode);
+  }
 
   const months = dom?.querySelectorAll('#main article ul')
 
